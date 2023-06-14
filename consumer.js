29f7c8af78e98713080ba5fbe7a5e5dcccf9a48e
Mert POLAT <90008958+mert2m@users.noreply.github.com>
2023-06-14 18:15:02 +0300

    Update consumer.js

--- consumer.js
+++ consumer.js
@@ -1,34 +1,32 @@
 const amqp = require("amqplib");
 const queueName = process.argv[2] || "jobsQueue";
-const data = require("./data.json")
+const data = require("./data.json");
+const redis = require("redis");
+const client = redis.createClient();
 
+connectRabbitMQ();
 
-
-connect_rabbitmq();
-
-async function connect_rabbitmq() {
+async function connectRabbitMQ() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     const assertion = await channel.assertQueue(queueName);
-    const redis = require("redis")
-    const client = redis.createClient();
+
     // Mesajın Alınması...
     console.log("Mesaj bekleniyor...");
-    channel.consume(queueName, message => {
-        const messageInfo = JSON.parse(message.content.toString())
-        //   {description : 12}
-        const userInfo = data.find(u => u.id == messageInfo.description)
-        if(userInfo){
-            console.log("İşlenen Kayıt", userInfo);
-            client.set('user_${userInfo.id}', JSON.stringify(userInfo), 
-            (err, status) => {
-              if(!err){
-              console.log("Status",status);
-              channel.ack(message);
+    channel.consume(queueName, async (message) => {
+      const messageInfo = JSON.parse(message.content.toString());
+      const userInfo = data.find((u) => u.id == messageInfo.description);
 
-             }})
-        }
+      if (userInfo) {
+        console.log("İşlenen Kayıt", userInfo);
+        client.set(`user_${userInfo.id}`, JSON.stringify(userInfo), (err, status) => {
+          if (!err) {
+            console.log("Status", status);
+            channel.ack(message);
+          }
+        });
+      }
     });
   } catch (error) {
     console.log("Error", error);
